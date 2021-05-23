8c201cc55e22b467bafc17d4de2166e260ce1fde
Alex White <alexlloydwhite@gmail.com>
2021-05-22 19:02:01 -0500

    adding comments..

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,27 +3,33 @@ import axios from 'axios'
 
 import './App.css'
 
+// Async function used to post images
 async function postImage({ image, description }) {
+  // initing form data
   const formData = new FormData();
   formData.append("image", image)
   formData.append("description", description)
-
+  // Sending formdata to the server
   const result = await axios.post('/images', formData, { headers: { 'Content-Type': 'multipart/form-data' } })
+  // returning results
   return result.data
 }
 
 function App() {
+  // Local State
   const [file, setFile] = useState()
   const [description, setDescription] = useState("")
   const [images, setImages] = useState([])
-
+  // Function to handle submit event
   const submit = async event => {
     event.preventDefault()
+    // Async - on submit use function to post image
     const result = await postImage({ image: file, description })
     console.log(result);
+    // Add the image to the images array
     setImages([result.imagePath, ...images])
   }
-
+  // function to handle file select
   const fileSelected = event => {
     const file = event.target.files[0]
     setFile(file)
